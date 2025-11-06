893e4dcdbc78b29f643947d0afafa31f2a30c4ca
davibatschke <davibatschkedev@gmail.com>
2025-11-06 15:05:31 -0300

    novidades no projeto do curso

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from galeria.models import Fotografia
+
+class ListandoFotos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda', 'publicacao')
+    list_display_links = ('id', 'nome')
+    search_fields = ['nome']
+    list_filter = ['categoria']
+    list_per_page = 10
 
 # Register your models here.
+admin.site.register(Fotografia, ListandoFotos)
